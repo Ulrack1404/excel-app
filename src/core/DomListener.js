1777c34c97461bf6d644039b756bfb2b52d79696
Denis <peredelkin.d@gmail.com>
2023-09-19 22:39:25 +0300

    Finish base framework functional

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -17,10 +17,16 @@ export class DomListener {
                     in ${this.name} Component`
                 );
             }
-            this.$root.on(listener, this[method].bind(this));
+            this[method] = this[method].bind(this);
+            this.$root.on(listener, this[method]);
         });
     }
-    removeDOMListeners() {}
+    removeDOMListeners() {
+        this.listeners.forEach((listener) => {
+            const method = getMethodName(listener);
+            this.$root.off(listener, this[method]);
+        });
+    }
 }
 
 function getMethodName(eventName) {
